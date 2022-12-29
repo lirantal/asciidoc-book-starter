5f119297d07fd4f5867e8abfeb3980e5b8a272f7
Liran Tal <liran.tal@gmail.com>
2022-12-29 16:42:09 +0200

    feat: update scripts so that book source directory and book generated directory differ

--- create-book-epub.sh
+++ create-book-epub.sh
@@ -2,5 +2,11 @@
 # Docker image: https://hub.docker.com/r/asciidoctor/docker-asciidoctor
 # Docker image repository: https://github.com/asciidoctor/docker-asciidoctor
 
+# The directory where the book source is located
+BOOK_SOURCE_DIR=book
+# The directory where the book's generated output files will be created
+BOOK_BUILD_DIR=build
+
 docker run --rm -v $(pwd)/book:/documents/ asciidoctor/docker-asciidoctor asciidoctor-epub3 \
-    index.adoc
+    -D $BOOK_BUILD_DIR \
+    $BOOK_SOURCE_DIR/index.adoc
--- create-book-pdf.sh
+++ create-book-pdf.sh
@@ -2,8 +2,14 @@
 # Docker image: https://hub.docker.com/r/asciidoctor/docker-asciidoctor
 # Docker image repository: https://github.com/asciidoctor/docker-asciidoctor
 
-docker run --rm -v $(pwd)/book:/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf \
-    -a pdf-themesdir=themes \
+# The directory where the book source is located
+BOOK_SOURCE_DIR=book
+# The directory where the book's generated output files will be created
+BOOK_BUILD_DIR=build
+
+docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf \
+    -a pdf-themesdir=$BOOK_SOURCE_DIR/themes \
     -a pdf-theme=basic \
-    -a pdf-fontsdir=fonts \
-    index.adoc
+    -a pdf-fontsdir=$BOOK_SOURCE_DIR/fonts \
+    -D $BOOK_BUILD_DIR \
+    $BOOK_SOURCE_DIR/index.adoc
