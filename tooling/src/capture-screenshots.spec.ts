5b2f4e25a49ce089979687088285c06947b16588
Liran Tal <liran.tal@gmail.com>
2023-01-01 20:45:37 +0200

    feat: add logic to take a screenshot of the snyk advisor package page and specifically the popularity section

--- tooling/src/capture-screenshots.spec.ts
+++ tooling/src/capture-screenshots.spec.ts
@@ -1,1 +1,27 @@
-console.log('hello world');
+import { test, expect } from '@playwright/test';
+
+test('take screenshot of element', async ({ page }) => {
+  
+  await page.goto('https://snyk.io/advisor/npm-package/playwright');
+
+  // sanity tests
+  await expect(page).toHaveTitle(/Playwright/ig);
+
+  const element = await page.locator('#popularity');
+  await page.waitForSelector('#line-chart');
+  await page.waitForLoadState('domcontentloaded');
+  await page.waitForLoadState('networkidle');
+
+  await page.hover('.vue--popularity-chart-dropdown__value');
+  await page.locator('.vue--popularity-chart-dropdown__value').click({force: true});
+  await page.waitForSelector('.vue--popularity-chart-dropdown__options', { state: 'attached' });
+
+  await page.locator('.vue--popularity-chart-dropdown__options').locator('label').nth(0).click();
+
+  await page.locator('.vue--popularity-chart-dropdown__value').click({force: true});
+  await page.waitForSelector('.vue--popularity-chart-dropdown__options', { state: 'attached' });
+  await page.locator('label').filter({ hasText: 'Download trend' }).click({force: true});
+
+  await element.screenshot({ path: 'figure1-1.png' });
+
+});
