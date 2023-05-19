f3653c458e6c1cf02cc9beb7907badfa7da0b130
Liran Tal <liran.tal@gmail.com>
2023-05-19 10:40:47 +0300

    Update create-book-pdf.sh

--- create-book-pdf.sh
+++ create-book-pdf.sh
@@ -6,7 +6,11 @@
 BOOK_SOURCE_DIR=book
 # The directory where the book's generated output files will be created
 BOOK_BUILD_DIR=build
-
+#     To override theme configuration file you can pass the following command-line arguments:
+#
+#    -a pdf-themesdir=$BOOK_SOURCE_DIR/themes \
+#    -a pdf-theme=$1 \
+#    -a pdf-fontsdir=$BOOK_SOURCE_DIR/fonts \
 docker run --rm -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf \
     -D $BOOK_BUILD_DIR \
     $BOOK_SOURCE_DIR/index.adoc
